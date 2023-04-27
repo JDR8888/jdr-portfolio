6997dba17b014c76ab39060b5b05ab634c3f3186
jake rudlong <jrudlong@gmail.com>
2023-04-27 12:54:55 -0500

    adds updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf';
-import myPDF from '../assets/2023-resume-short-jdr.pdf';
+import myPDF from '../assets/2023-resume-jdr-v2.pdf';
 
 function Resume() {
   
   return (
     <div>
-      {/* <Document file={myPDF}>
-        <Page pageNumber={1} />
-      </Document> */}
+
       <object width="100%" height="800" data={myPDF} type="application/pdf">   </object>
 
     </div>
