b93f7c5c08ec3f60b46ec5e61979a38b0917acfe
jake rudlong <jrudlong@gmail.com>
2023-05-02 12:55:48 -0500

    updated resume v3

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf';
-import myPDF from '../assets/2023-resume-jdr-v2.pdf';
+import myPDF from '../assets/2023-resume-jdr-v3.pdf';
 
 function Resume() {
   
